f41294a26f5888aae08b2eeba692f0583f1b5cd9
zsedelypeter <zsedely.peter@gmail.com>
2024-10-09 19:21:26 +0200

    Not yet complete launch file and node added

---
+++ launch/MitsuLaunch.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim',
+        ),
+        Node(
+            package='zse_exn_arj',
+            executable='Mitsu',
+            output='screen',
+        ),
+    ])
